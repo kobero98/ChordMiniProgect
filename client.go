d518b28761b9529d2b6a745f922176543dd82a23
kobero98 <matteo.fede@live.it>
2023-01-18 00:07:24 +0100

    aggiunto controllo parametri avvio client e aggiunto file eseguibile

--- client.go
+++ client.go
@@ -9,7 +9,17 @@ import (
 )
 
 func main() {
-
+	if len(os.Args) != 2 {
+		fmt.Println("esseguire il programma:")
+		fmt.Println("./client 1 val per immagazzinare una stringa")
+		fmt.Println("./client 0 key per ottenere la stringa su una determinata chiave")
+		return
+	}
+	x, errconv := strconv.Atoi(os.Args[1])
+	if errconv != nil || (x != 0 && x != 1) {
+		log.Fatal("error valore non valido selezionare o 0 o 1")
+		return
+	}
 	client, err := rpc.DialHTTP("tcp", "register:8000")
 	if err != nil {
 		log.Fatal("dialing:", err)
@@ -30,17 +40,18 @@ func main() {
 	if err1 != nil {
 		log.Fatal("dialing:", err)
 	}
-	x, _ := strconv.Atoi(os.Args[2])
 	if x == 1 {
-		parola = os.Args[1]
+		parola = os.Args[2]
 		fmt.Println(parola)
 		err = client1.Call("ChordNode.Put", &parola, &reply)
 		if err != nil {
 			log.Fatal("arith error:", err)
 		}
 		fmt.Println("risposta: ", reply)
-	} else {
-		reply, _ = strconv.Atoi(os.Args[1])
+	}
+
+	if x == 0 {
+		reply, _ = strconv.Atoi(os.Args[2])
 		var parola2 string
 		err = client1.Call("ChordNode.Get", &reply, &parola2)
 		if err != nil {
